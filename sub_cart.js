0fc998ebaeef26f2c5aa684887de6b1170593d4c
Khalel Abaquin <kabaquin694@west-mec.org>
2019-04-05 15:26:22 -0700

    Update 3
    added final comments, fixed (most) broken code, added a function, added local variables, added a for loop, & added two if statements

--- sub_cart.js
+++ sub_cart.js
@@ -23,19 +23,39 @@
 */
 
 //This line of code loads the function 'setupCart' every time the browser loads.
-window.addEventListener("load", setupCart);
+window.addEventListener("load", setupCart); //???
 
-//?
+//This function grabs the elements with the class of 'addButton' & stores it in the variable of 'addButtons'. An event listener is then added to 'addButtons' inside a for loop.
 function setupCart() {
       var addButtons = document.getElementsByClassName("addButton");
+      console.log(addButtons)
       for (var i = 0; i < addButtons.length; i++) {
-            addButtons.addEventListener("click", addItem); //???
+            addButtons[i].addEventListener("click", addItem);
       }
 }
 
-//?
-function addItem() {
-      // var foodItem = document.getElementById("").nextElementSibling;
-      var foodID = foodItem.getAttribute("id");
-      var foodDescription = foodItem.cloneNode();
+//The 'addItem' function adds items to the cart by having 5 local variables, a for loop, & 2 if statements.
+function addItem(e) {
+      var foodItem = e.target.nextElementSibling;
+      console.log(foodItem);
+      var foodID = foodItem.getAttribute("id").value;
+      var foodDescription = foodItem.cloneNode(true);
+      var cartBox = document.getElementById("cart");
+      var duplicateOrder = false;
+
+      //This section of code loops through the child nodes of 'cartBox' & if the id of the child nodes match with the id of 'foodID', then the value of the first child node of 'cartBox' will increase by 1.
+      for (var i = 0; i < cartBox.childNodes.length; i++) {
+            if (cartBox.childNodes.id === foodID) {
+                  cartBox.firstChild.value++ //here??
+            }
+            break;
+      }
+
+      //This block of code checks to see if 'duplicateOrder' is false. If so, the text content of 'orderCount' will change to 1, 'orderCount' will be appended to 'foodDescription', & 'foodDescription' would be appended to 'cartBox'.
+      if (duplicateOrder === false) {
+            var orderCount = document.createElement("span");
+            orderCount.textContent = 1;
+            foodDescription.appendChild(orderCount);
+            cartBox.appendChild(foodDescription);
+      }
 }
